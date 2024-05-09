752767647da1a25d2591a31914e9b7328e0933d7
Brian <thinhle1199@gmail.com>
2024-05-08 19:40:18 -0500

    first commit

---
+++ src/main/java/com/brian/springbootapiwithangular/SpringBootApiWithAngularApplication.java
@@ -0,0 +1,13 @@
+package com.brian.springbootapiwithangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiWithAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brian/springbootapiwithangular/SpringBootApiWithAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brian.springbootapiwithangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiWithAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
