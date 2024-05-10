999d1a8c048a93a1fa313ef36b2906bfc79bd616
Brian <thinhle1199@gmail.com>
2024-05-09 21:56:22 -0500

    handle exception when cannot find user by id

--- src/main/java/com/brian/springbootapiwithangular/repo/EmployeeRepo.java
+++ src/main/java/com/brian/springbootapiwithangular/repo/EmployeeRepo.java
@@ -3,5 +3,10 @@ package com.brian.springbootapiwithangular.repo;
 import com.brian.springbootapiwithangular.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    public void deleteEmployeeById(Long id);
+    public Optional<Employee> findEmployeeById(Long id);
+
 }
--- src/main/java/com/brian/springbootapiwithangular/service/EmployeeService.java
+++ src/main/java/com/brian/springbootapiwithangular/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.brian.springbootapiwithangular.service;
 
+import com.brian.springbootapiwithangular.exception.UserNotFoundException;
 import com.brian.springbootapiwithangular.repo.EmployeeRepo;
 import com.brian.springbootapiwithangular.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,21 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id" + id + " was not found"));
+    }
+
+    public void deleteEmployee(long id){
+        employeeRepo.deleteEmployeeById(id);
     }
 }
